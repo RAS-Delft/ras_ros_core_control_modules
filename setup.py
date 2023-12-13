5bd88e86dbf8333c1c4fa0d873abcb936cc0d3ba
Bart <bartboogmans@hotmail.com>
2023-12-13 11:26:10 +0100

    Update dependencies

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'ras_ros_core_control_modules'
 
@@ -16,6 +18,16 @@ setup(
         ## BB I have no clue why python wants the items to be installed in this specific location.
         ## All I know is that if installed in this location, other ros packages can find them.
         # I guess it is just moving things from source to install space, although (specifically) the python files themselves do not need building. 
+
+        # import python package
+        #(os.path.join('lib',package_name,'control_effort_allocation'), glob(os.path.join(package_name,'control_effort_allocation','*.py'))),
+        #(os.path.join('lib',package_name,'motion_control'), glob(os.path.join(package_name,'motion_control','*.py'))),
+        #(os.path.join('lib',package_name,'sensor_processing'), glob(os.path.join(package_name,'sensor_processing','*.py'))),
+        #(os.path.join('lib','python3.8',package_name,'tools'), glob(os.path.join(package_name,'control_effort_allocation','*.py'))),
+
+        # import all launch files
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+        
         ('lib/python3.8/site-packages/' + package_name + '/tools', [package_name+'/tools/geometry_tools.py',
                                                                     package_name+'/tools/titoneri_parameters.py',
                                                                     package_name+'/tools/display_tools.py',
@@ -23,7 +35,7 @@ setup(
         ('lib/python3.8/site-packages/' + package_name + '/sensor_processing', [package_name+'/sensor_processing/vel_differentiator.py',package_name+'/sensor_processing/navsat_cartesian_transform.py']),
         ('lib/python3.8/site-packages/' + package_name + '/motion_control', [package_name+'/motion_control/heading_controller.py', 
                                                                              package_name+'/motion_control/surge_vel_controller.py']),
-        ('lib/python3.8/site-packages/' + package_name + '/control_effort_allocation', [package_name+'/control_effort_allocation/TN_control_effort_allocator_nomoto.py']),
+        ('lib/python3.8/site-packages/' + package_name + '/control_effort_allocation', [package_name+'/control_effort_allocation/TN_control_effort_allocator_nomoto.py']), 
     ],
     
     install_requires=['setuptools'],
