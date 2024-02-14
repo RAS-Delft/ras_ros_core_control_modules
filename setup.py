1a4e19aeb0b33053c6b8630144b4c64017bd418d
Bart <bartboogmans@hotmail.com>
2024-02-14 15:57:18 +0100

    Correct launch file naming

--- setup.py
+++ setup.py
@@ -13,20 +13,8 @@ setup(
 
         ('share/ament_index/resource_index/packages',['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, ['launch/fleet_sim.launch.py','launch/vessel_sim.launch.py']),
+        ('share/' + package_name, ['launch/fleet_sim.launch.py','launch/fleet_geo_utils.launch.py']),
 
-        ## BB I have no clue why python wants the items to be installed in this specific location.
-        ## All I know is that if installed in this location, other ros packages can find them.
-        # I guess it is just moving things from source to install space, although (specifically) the python files themselves do not need building. 
-
-        # import python package
-        #(os.path.join('lib',package_name,'control_effort_allocation'), glob(os.path.join(package_name,'control_effort_allocation','*.py'))),
-        #(os.path.join('lib',package_name,'motion_control'), glob(os.path.join(package_name,'motion_control','*.py'))),
-        #(os.path.join('lib',package_name,'sensor_processing'), glob(os.path.join(package_name,'sensor_processing','*.py'))),
-        #(os.path.join('lib','python3.8',package_name,'tools'), glob(os.path.join(package_name,'control_effort_allocation','*.py'))),
-
-        # import all launch files
-        #(os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
         
         ('lib/python3.8/site-packages/' + package_name + '/tools', [package_name+'/tools/geometry_tools.py',
                                                                     package_name+'/tools/titoneri_parameters.py',
