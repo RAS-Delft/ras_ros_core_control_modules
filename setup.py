e82957ac4919fedf11ce92313d22b70ed7f7a6e6
Bart <bartboogmans@hotmail.com>
2024-02-27 14:41:28 +0100

    edit dependencies

--- setup.py
+++ setup.py
@@ -20,10 +20,13 @@ setup(
                                                                     package_name+'/tools/titoneri_parameters.py',
                                                                     package_name+'/tools/display_tools.py',
                                                                     package_name+'/tools/control_tools.py']),
-        ('lib/python3.8/site-packages/' + package_name + '/sensor_processing', [package_name+'/sensor_processing/vel_differentiator.py',package_name+'/sensor_processing/navsat_cartesian_transform.py']),
+        ('lib/python3.8/site-packages/' + package_name + '/sensor_processing', [package_name+'/sensor_processing/vel_differentiator.py',
+                                                                                package_name+'/sensor_processing/navsat_cartesian_transform.py']),
         ('lib/python3.8/site-packages/' + package_name + '/motion_control', [package_name+'/motion_control/heading_controller.py', 
                                                                              package_name+'/motion_control/surge_vel_controller.py']),
-        ('lib/python3.8/site-packages/' + package_name + '/control_effort_allocation', [package_name+'/control_effort_allocation/TN_control_effort_allocator_nomoto.py']), 
+        ('lib/python3.8/site-packages/' + package_name + '/control_effort_allocation', [package_name+'/control_effort_allocation/TN_control_effort_allocator_nomoto.py'
+                                                                                        ,package_name+'/control_effort_allocation/TN_control_effort_allocator_dp_01.py'
+                                                                                        ,package_name+'/control_effort_allocation/control_effort_allocator_super.py']), 
     ],
     
     install_requires=['setuptools'],
@@ -40,6 +43,7 @@ setup(
             'heading_controller = ras_ros_core_control_modules.motion_control.heading_controller:main',
             'surge_vel_controller = ras_ros_core_control_modules.motion_control.surge_vel_controller:main',
             'TN_control_effort_allocator_nomoto = ras_ros_core_control_modules.control_effort_allocation.TN_control_effort_allocator_nomoto:main',
+            'TN_control_effort_allocator_dp1 = ras_ros_core_control_modules.control_effort_allocation.TN_control_effort_allocator_dp_01:main',
         ],
     },
 )
