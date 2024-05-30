deff746ee0f38b92561ad2acfa25934a0b6b44c0
Bart <bartboogmans@hotmail.com>
2024-05-30 20:42:48 +0200

    bugfix in launchfile, return missing

--- launch/fleet_sim.launch.py
+++ launch/fleet_sim.launch.py
@@ -1,26 +1,19 @@
-'''
-    Starts the specified simulations representing the vessels in the fleet
-'''
-
-from launch_ros.actions import Node
 from launch import LaunchDescription
+from launch_ros.actions import Node
 
 def generate_launch_description(vesselids = ['RAS_TN_DB','RAS_TN_OR','RAS_TN_GR']):
     ld = LaunchDescription()
-
-    # Define the vesselids to be simulated
-    #vesselids = ['RAS_TN_DB','RAS_TN_RE','RAS_TN_GR','RAS_TN_OR','RAS_TN_LB','RAS_TN_YE','RAS_TN_PU']
+    vesselids = ['RAS_TN_DB','RAS_TN_OR','RAS_TN_GR','RAS_TN_YE','RAS_TN_RD','RAS_TN_LB','RAS_TN_PU']
     
-    # Launch vesselstack.launch.py for each vesselid
+    # Bring up the control stack for each vessel
     for vesselid in vesselids:
         turtleboat_node = Node(
             package='turtleboat',
             executable='turtleboatmain',
             name='turtleboat_sim',
             namespace=vesselid,
-            parameters=[{'rate_publish_position':20.0},
-                        {'rate_publish_heading':17.0}],
             output='screen',
             emulate_tty=True,
         )
         ld.add_action(turtleboat_node)
+    return ld
