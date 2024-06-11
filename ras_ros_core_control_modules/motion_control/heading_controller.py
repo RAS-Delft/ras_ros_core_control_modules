73338289f83a61bf9dc5f756883cffbbfa1ed226
Bart <bartboogmans@hotmail.com>
2024-06-11 17:21:36 +0200

    remove argparse remains
    fully migrated heading control and surge control node to use ros2 params

--- ras_ros_core_control_modules/motion_control/heading_controller.py
+++ ras_ros_core_control_modules/motion_control/heading_controller.py
@@ -2,7 +2,6 @@
 
 import rclpy
 from rclpy.node import Node
-import argparse
 from std_msgs.msg import Float32, Float32MultiArray
 import ras_ros_core_control_modules.tools.display_tools as ras_display_tools
 from ras_ros_core_control_modules.tools.control_tools import PIDController
@@ -10,113 +9,117 @@ import ras_ros_core_control_modules.tools.geometry_tools as ras_geometry_tools
 from rclpy.qos import QoSProfile
 from rclpy.qos import QoSReliabilityPolicy, QoSHistoryPolicy, QoSDurabilityPolicy
 
-# Get arguments
-parser = argparse.ArgumentParser(description='ROS2 control module for heading control')
-parser.add_argument("objectID", type=str,help="set vessel identifier")
-parser.add_argument('-r') # ROS2 arguments
-args, unknown = parser.parse_known_args()
-
-# Set constants
-OBJECT_ID = args.objectID
 PERIOD_BROADCAST_STATUS = 5.0 # [s]
 
 class PIDController_rotationally(PIDController):
-	"""
-	Subclass of PIDController with variation that calculates the error in a rotational sense. 
-	This means that the error is calculated as the shortest angle between the reference and the state.
-	"""
-	def calc_error(self):
-		"""
-		Calculate the error in a rotational sense
-		Overwrites the (linear) calc_error method of the PIDController class to be rotationally continuous between 0 and 2pi
-		"""
-		return ras_geometry_tools.signed_shortest_angle_radians(self.ref,self.state)
+    """
+    Subclass of PIDController with variation that calculates the error in a rotational sense. 
+    This means that the error is calculated as the shortest angle between the reference and the state.
+    """
+    def calc_error(self):
+        """
+        Calculate the error in a rotational sense
+        Overwrites the (linear) calc_error method of the PIDController class to be rotationally continuous between 0 and 2pi
+        """
+        return ras_geometry_tools.signed_shortest_angle_radians(self.ref,self.state)
 
 class HeadingControllerNode(Node):
-	def __init__(self):
-		super().__init__('heading_controller')
-		custom_qos_profile = QoSProfile(
-    		reliability=QoSReliabilityPolicy.BEST_EFFORT,
-    		history=QoSHistoryPolicy.KEEP_LAST,
-    		depth=1,
-    		durability=QoSDurabilityPolicy.VOLATILE
-		)
+    def __init__(self):
+        super().__init__('heading_controller')
+        custom_qos_profile = QoSProfile(
+            reliability=QoSReliabilityPolicy.BEST_EFFORT,
+            history=QoSHistoryPolicy.KEEP_LAST,
+            depth=1,
+            durability=QoSDurabilityPolicy.VOLATILE
+        )
 
-		# create PID controller
-		self.headingPID = PIDController_rotationally(0.32,0.0,0.0)# ki = 0.0962
-		self.headingPID.output_limits=[-0.63,0.63] # [N*m] NEW, referring to desired torque output
-		self.headingPID.integral_limits = [-1.0,1.0]
-		
-		# Set up publishers and subscribers
-		self.subscriber_reference = self.create_subscription(Float32,'reference/heading',self.callback_reference,custom_qos_profile)
-		self.subscriber_state = self.create_subscription(Float32,'telemetry/heading',self.callback_state,custom_qos_profile)
-		self.publisher_pid_status = self.create_publisher(Float32MultiArray,'diagnostics/headingController/pid_status',custom_qos_profile)
-		self.publisher_desired_torque = self.create_publisher(Float32,'reference/controlEffort/torqueZ',custom_qos_profile)
+        # Set up ros parameters
+        self.declare_parameter('period_broadcast_status',5.0)
+        self.declare_parameter('enable_report_pid_status',False)
 
-		# Statistics
-		self.timer_statistics = self.create_timer(PERIOD_BROADCAST_STATUS, self.print_statistics)
-		self.timer_statistics_last = self.get_clock().now().nanoseconds/1e9
-		self.tracker_callback_reference = 0
-		self.tracker_callback_state = 0
-		self.tracker_callback_control = 0
-	
-	def callback_reference(self,msg:Float32):
-		self.tracker_callback_reference += 1
-		self.headingPID.setRef(msg.data)
-		self.run_controls()
+        # create PID controller
+        self.headingPID = PIDController_rotationally(0.32,0.0,0.0)# ki = 0.0962
+        self.headingPID.output_limits=[-0.63,0.63] # [N*m] NEW, referring to desired torque output
+        self.headingPID.integral_limits = [-1.0,1.0]
+        
+        # Set up publishers and subscribers
+        self.subscriber_reference = self.create_subscription(Float32,'reference/heading',self.callback_reference,custom_qos_profile)
+        self.subscriber_state = self.create_subscription(Float32,'telemetry/heading',self.callback_state,custom_qos_profile)
+        self.publisher_desired_torque = self.create_publisher(Float32,'reference/controlEffort/torqueZ',custom_qos_profile)
+        
+        if self.get_parameter('enable_report_pid_status').value:
+            self.publisher_pid_status = self.create_publisher(Float32MultiArray,'diagnostics/headingController/pid_status',custom_qos_profile)
+        else:
+            self.publisher_pid_status = None
 
-	def callback_state(self,msg:Float32):
-		self.tracker_callback_state += 1
-		self.headingPID.setState(msg.data)
-		self.run_controls()
-	
-	def run_controls(self):
-		# Update tracker
-		self.tracker_callback_control += 1
+        # Statistics
+        self.timer_statistics = self.create_timer(self.get_parameter('period_broadcast_status').get_parameter_value().double_value, self.print_statistics)
+        self.timer_statistics_last = self.get_clock().now().nanoseconds/1e9
+        self.tracker_callback_reference = 0
+        self.tracker_callback_state = 0
+        self.tracker_callback_control = 0
+    
+    def callback_reference(self,msg:Float32):
+        self.tracker_callback_reference += 1
+        self.headingPID.setRef(msg.data)
+        self.run_controls()
 
-		# Run PID controller
-		desired_torque = self.headingPID.compute() 
+    def callback_state(self,msg:Float32):
+        self.tracker_callback_state += 1
+        self.headingPID.setState(msg.data)
+        self.run_controls()
+    
+    def run_controls(self):
+        # Update tracker
+        self.tracker_callback_control += 1
 
-		# Publish actuation
-		msg = Float32(data=desired_torque)
-		self.publisher_desired_torque.publish(msg)
+        # Run PID controller
+        desired_torque = self.headingPID.compute() 
 
-	def print_statistics(self):
-		""" On a single line, print the rates of all major callbacks in this script. """
+        # Publish actuation
+        msg = Float32(data=desired_torque)
+        self.publisher_desired_torque.publish(msg)
 
-		# Calculate passed time
-		now = self.get_clock().now().nanoseconds/1e9
-		passed_time = now - self.timer_statistics_last
+    def print_statistics(self):
+        """ On a single line, print the rates of all major callbacks in this script. """
 
-		# Calculate rates up to two decimals
-		rate_reference = round(self.tracker_callback_reference/passed_time,2)
-		rate_state = round(self.tracker_callback_state/passed_time,2)
-		rate_control = round(self.tracker_callback_control/passed_time,2)
+        # Calculate passed time
+        now = self.get_clock().now().nanoseconds/1e9
+        passed_time = now - self.timer_statistics_last
 
-		# Format information to string
-		printstring = ras_display_tools.terminal_fleet_module_string(self.get_namespace()[1:], ['reference_rate',rate_reference,'hz'],['state_rate',rate_state,'hz'],['control_rate',rate_control,'hz'])
+        # Calculate rates up to two decimals
+        rate_reference = round(self.tracker_callback_reference/passed_time,2)
+        rate_state = round(self.tracker_callback_state/passed_time,2)
+        rate_control = round(self.tracker_callback_control/passed_time,2)
 
-		# Print
-		self.get_logger().info(printstring)
+        # Format information to string
+        printstring = ras_display_tools.terminal_fleet_module_string(self.get_namespace()[1:], ['reference_rate',rate_reference,'hz'],['state_rate',rate_state,'hz'],['control_rate',rate_control,'hz'])
 
-		# Reset trackers
-		self.tracker_callback_reference = 0
-		self.tracker_callback_state = 0
-		self.tracker_callback_control = 0
-		self.timer_statistics_last = now
+        # Print
+        self.get_logger().info(printstring)
+
+        # Reset trackers
+        self.tracker_callback_reference = 0
+        self.tracker_callback_state = 0
+        self.tracker_callback_control = 0
+        self.timer_statistics_last = now
 
 def main(args=None):
-	rclpy.init(args=args)
+    rclpy.init(args=args)
 
-	node = HeadingControllerNode()
+    node = HeadingControllerNode()
 
-	# Start the nodes processing thread
-	rclpy.spin(node)
-
-	# at termination of the code (generally with ctrl-c) Destroy the node explicitly
-	node.destroy_node()
-	rclpy.shutdown()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        print(" Stopping cleanly")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+    finally:
+        if rclpy.ok():
+            node.destroy_node()
+            rclpy.shutdown()
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- ras_ros_core_control_modules/motion_control/surge_vel_controller.py
+++ ras_ros_core_control_modules/motion_control/surge_vel_controller.py
@@ -3,7 +3,6 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32, Float32MultiArray
-import argparse
 
 from ras_ros_core_control_modules.tools.control_tools import PIDController
 import ras_ros_core_control_modules.tools.display_tools as ras_display_tools
@@ -11,104 +10,104 @@ import ras_ros_core_control_modules.tools.display_tools as ras_display_tools
 from rclpy.qos import QoSProfile
 from rclpy.qos import QoSReliabilityPolicy, QoSHistoryPolicy, QoSDurabilityPolicy
 
-
-# Get arguments
-parser = argparse.ArgumentParser(description='ROS2 control module for surge velocity control')
-parser.add_argument("objectID", type=str,help="set vessel identifier")
-parser.add_argument('-r') # ROS2 arguments
-args, unknown = parser.parse_known_args()
-
-# Set constants
-OBJECT_ID = args.objectID
-PERIOD_CONTROL = 1.0/16.0 # [s] 
-PERIOD_BROADCAST_STATUS = 5.0 # [s]
-PERIOD_REPORT_PID_STATUS = 1.0 # [s]
-
 class SurgeVelControllerNode(Node):
-	def __init__(self):
-		super().__init__('surge_vel_controller')
-		custom_qos_profile = QoSProfile(
-    		reliability=QoSReliabilityPolicy.BEST_EFFORT,
-    		history=QoSHistoryPolicy.KEEP_LAST,
-    		depth=1,
-    		durability=QoSDurabilityPolicy.VOLATILE
-		)
+    def __init__(self):
+        super().__init__('surge_vel_controller')
+        custom_qos_profile = QoSProfile(
+            reliability=QoSReliabilityPolicy.BEST_EFFORT,
+            history=QoSHistoryPolicy.KEEP_LAST,
+            depth=1,
+            durability=QoSDurabilityPolicy.VOLATILE
+        )
 
-		# Make PID controllers
-		self.PID = PIDController(21.0,0.0,0.0) # kp was 21.0 kd was 2.55
-		self.PID.integral_limits=[-8*0.25, 8*0.25] # set limit to a total buildup of 8 seconds with a characteristic error of 0.25m/s/s
-		self.PID.output_limits=[0.1,3.0] # Newtons of thrust from both aft thrusters combined
+        # Set up ros parameters
+        self.declare_parameter('period_control',1.0/16.0)
+        self.declare_parameter('period_broadcast_status',5.0)
+        self.declare_parameter('enable_report_pid_status',False)
 
-		# Set up publishers and subscribers
-		self.publisher_forceX = self.create_publisher(Float32,'reference/controlEffort/forceX',custom_qos_profile)
-		self.subscriber_reference = self.create_subscription(Float32MultiArray,'reference/velocity',self.reference_callback,custom_qos_profile)
-		self.subscriber_state = self.create_subscription(Float32MultiArray,'state/velocity',self.state_callback,custom_qos_profile)
-		self.publisher_pid_status = self.create_publisher(Float32MultiArray,'diagnostics/surgeVelocityController/pid_status',custom_qos_profile)
+        # Make PID controllers
+        self.PID = PIDController(21.0,0.0,0.0) # kp was 21.0 kd was 2.55
+        self.PID.integral_limits=[-8*0.25, 8*0.25] # set limit to a total buildup of 8 seconds with a characteristic error of 0.25m/s/s
+        self.PID.output_limits=[0.1,3.0] # Newtons of thrust from both aft thrusters combined
 
-		# Set up timer for control loop
-		self.timer_control = self.create_timer(PERIOD_CONTROL, self.run_controls)
+        # Set up publishers and subscribers
+        self.publisher_forceX = self.create_publisher(Float32,'reference/controlEffort/forceX',custom_qos_profile)
+        self.subscriber_reference = self.create_subscription(Float32MultiArray,'reference/velocity',self.reference_callback,custom_qos_profile)
+        self.subscriber_state = self.create_subscription(Float32MultiArray,'state/velocity',self.state_callback,custom_qos_profile)
+        
+        if self.get_parameter('enable_report_pid_status').value:
+            self.publisher_pid_status = self.create_publisher(Float32MultiArray,'diagnostics/surgeVelocityController/pid_status',custom_qos_profile)
+        else:
+            self.publisher_pid_status = None
 
-		# Set up statistics
-		self.timer_statistics = self.create_timer(PERIOD_BROADCAST_STATUS, self.print_statistics)
-		self.timer_statistics_last = self.get_clock().now().nanoseconds/1e9
-		self.tracker_callback_reference = 0
-		self.tracker_callback_state = 0
-		self.tracker_callback_control = 0
+        # Set up timer for control loop
+        self.timer_control = self.create_timer(self.get_parameter('period_control').get_parameter_value().double_value, self.run_controls)
 
-	def reference_callback(self, msg:Float32MultiArray):
-		self.tracker_callback_reference += 1
-		self.PID.setRef(msg.data[0])
+        # Set up statistics
+        self.timer_statistics = self.create_timer(self.get_parameter('period_broadcast_status').get_parameter_value().double_value, self.print_statistics)
+        self.timer_statistics_last = self.get_clock().now().nanoseconds/1e9
+        self.tracker_callback_reference = 0
+        self.tracker_callback_state = 0
+        self.tracker_callback_control = 0
 
-	def state_callback(self, msg:Float32MultiArray):
-		self.tracker_callback_state += 1
-		self.PID.setState(msg.data[0])
-	
-	def run_controls(self):
-		# add to rate tracker
-		self.tracker_callback_control += 1
-		
-		# Calculate PID output
-		out = self.PID.compute()
+    def reference_callback(self, msg:Float32MultiArray):
+        self.tracker_callback_reference += 1
+        self.PID.setRef(msg.data[0])
 
-		# Send message
-		msg = Float32(data=out)
-		self.publisher_forceX.publish(msg)
+    def state_callback(self, msg:Float32MultiArray):
+        self.tracker_callback_state += 1
+        self.PID.setState(msg.data[0])
+    
+    def run_controls(self):
+        # add to rate tracker
+        self.tracker_callback_control += 1
+        
+        # Calculate PID output
+        out = self.PID.compute()
 
-	def print_statistics(self):
-		# Calculate passed time
-		now = self.get_clock().now().nanoseconds/1e9
-		passed_time = now - self.timer_statistics_last
+        # Send message
+        msg = Float32(data=out)
+        self.publisher_forceX.publish(msg)
 
-		# Calculate rates up to two decimals
-		rate_reference = round(self.tracker_callback_reference/passed_time,2)
-		rate_state = round(self.tracker_callback_state/passed_time,2)
-		rate_control = round(self.tracker_callback_control/passed_time,2)
+    def print_statistics(self):
+        # Calculate passed time
+        now = self.get_clock().now().nanoseconds/1e9
+        passed_time = now - self.timer_statistics_last
 
-		# Format information to string
-		printstring = ras_display_tools.terminal_fleet_module_string(self.get_namespace()[1:], ['reference_rate',rate_reference,'hz'],['state_rate',rate_state,'hz'],['control_rate',rate_control,'hz'])
+        # Calculate rates up to two decimals
+        rate_reference = round(self.tracker_callback_reference/passed_time,2)
+        rate_state = round(self.tracker_callback_state/passed_time,2)
+        rate_control = round(self.tracker_callback_control/passed_time,2)
 
-		# Print
-		self.get_logger().info(printstring)
+        # Format information to string
+        printstring = ras_display_tools.terminal_fleet_module_string(self.get_namespace()[1:], ['reference_rate',rate_reference,'hz'],['state_rate',rate_state,'hz'],['control_rate',rate_control,'hz'])
 
-		# Reset trackers
-		self.tracker_callback_reference = 0
-		self.tracker_callback_state = 0
-		self.tracker_callback_control = 0
-		self.timer_statistics_last = now
+        # Print
+        self.get_logger().info(printstring)
+
+        # Reset trackers
+        self.tracker_callback_reference = 0
+        self.tracker_callback_state = 0
+        self.tracker_callback_control = 0
+        self.timer_statistics_last = now
 
 def main(args=None):
-	rclpy.init(args=args)
+    rclpy.init(args=args)
 
-	node = SurgeVelControllerNode()
+    node = SurgeVelControllerNode()
 
-	# Start the nodes processing thread
-	rclpy.spin(node)
-
-	# at termination of the code (generally with ctrl-c) Destroy the node explicitly
-	node.destroy_node()
-	rclpy.shutdown()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        print(" Stopping cleanly")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+    finally:
+        if rclpy.ok():
+            node.destroy_node()
+            rclpy.shutdown()
 
 
 if __name__ == '__main__':
-	main()
-	
+    main()
+    
